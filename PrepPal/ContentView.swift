4513bc8ef5fbfa243a58015e6c7a67eef0bd3fa8
patilchirag2392 <patil.chirag@northeastern.edu>
2025-04-21 17:53:56 -0400

    Created Project

---
+++ PrepPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrepPal
+//
+//  Created by Chirag Patil on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrepPal/PrepPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepPalApp.swift
+//  PrepPal
+//
+//  Created by Chirag Patil on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PrepPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
